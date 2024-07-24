b8f019feb8d3d81614e20f79cb3345abdddf2a46
Sidd <sr.sanagala@outlook.com>
2024-07-25 01:22:15 +0800

    Added route to update user info

--- app.py
+++ app.py
@@ -43,5 +43,49 @@ def recommend():
 
     return jsonify(recommended_items)
 
+@app.route('/update', methods=['POST'])
+def update():
+    data = request.get_json()
+    new_user_data = data['new_user_data']
+    
+    user_ids = []
+    item_ids = []
+    labels = []
+
+    for user in new_user_data:
+        user_id = usernames.get(user["id"])
+        if user_id is None:
+            user_id = len(usernames)
+            usernames[user["id"]] = user_id
+
+        for item in user["liked_items"]:
+            item_id = clothes.get(item["productID"])
+            if item_id is None:
+                item_id = len(clothes)
+                clothes[item["productID"]] = item_id
+            user_ids.append(user_id)
+            item_ids.append(item_id)
+            labels.append(1)
+
+        for item in user["disliked_items"]:
+            item_id = clothes.get(item["productID"])
+            if item_id is None:
+                item_id = len(clothes)
+                clothes[item["productID"]] = item_id
+            user_ids.append(user_id)
+            item_ids.append(item_id)
+            labels.append(-1)
+
+    user_ids_array = np.array(user_ids)
+    item_ids_array = np.array(item_ids)
+    labels_array = np.array(labels)
+
+    model.fit([user_ids_array, item_ids_array], labels_array, epochs=1, verbose=1)
+
+    model.save('swipestyle-ai.keras')
+    
+    return jsonify({"message": "Model updated!"})
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=True) 
