b82c2c817be6c406bf0cf0f2231c2b68f8dbfe20
Sidd <sr.sanagala@outlook.com>
2024-07-29 19:15:10 +0800

    Finalized model training with new users

--- app.py
+++ app.py
@@ -1,71 +1,111 @@
 from flask import Flask, request, jsonify
 import numpy as np
-from tensorflow.keras.models import load_model
 from tensorflow.keras.models import load_model, Model
-from tensorflow.keras.layers import Embedding, Flatten, Dot, Input, Dropout
+from tensorflow.keras.layers import Embedding, Flatten, Dot, Input, Dropout, Concatenate, Dense
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.regularizers import l2
 
 import os
+import json
 from dotenv import load_dotenv, find_dotenv
 from supabase import create_client, Client
 
-from main import PreProcessData
+from main import FetchData
 
 app = Flask(__name__)
 
 # Load your model
 model_path = 'swipestyle-ai.keras'
-model = load_model(model_path)
+model = None
 
-# Load user and clothing data
-load_dotenv(find_dotenv())
+clothing_data = FetchData().clothing_data()
 
-supabase_url = os.getenv("SUPABASE_URL")
-supabase_key = os.getenv("SUPABASE_KEY")
-supabase_client: Client = create_client(supabase_url, supabase_key)
+with open('./data/usernames.json') as f:
+    usernames = json.load(f)
+with open('./data/clothes.json') as f:
+    clothes = json.load(f)
+with open('./data/brands.json') as f:
+    brands = json.load(f)
+with open('./data/genders.json') as f:
+    genders = json.load(f)
 
-user_data = supabase_client.from_('users').select('id').execute().data
-clothing_data = PreProcessData().fetch_clothing_data()
+def load_global_model():
+    global model
+    model = load_model(model_path)
 
-usernames = {user['id']: i for i, user in enumerate(user_data)}
-clothes = {item["productID"]: i for i, item in enumerate(clothing_data)}
+load_global_model()
 
-def create_model(num_users, num_items, embedding_size=50, reg_strength=1e-3, dropout_rate=0.5):
+
+def create_model(num_users, num_items, num_brands, num_genders, embedding_size=50, reg_strength=1e-3, dropout_rate=0.5):
     regularizer = l2(reg_strength)
+
+    # User input and embedding
     user_input = Input(shape=(1,))
-    user_embedding = Embedding(num_users, embedding_size, input_length=1, embeddings_regularizer=regularizer)(user_input)
+    user_embedding = Embedding(num_users, embedding_size, embeddings_regularizer=regularizer)(user_input)
     user_vector = Flatten()(user_embedding)
     user_vector = Dropout(dropout_rate)(user_vector)
 
+    # Item input and embedding
     item_input = Input(shape=(1,))
-    item_embedding = Embedding(num_items, embedding_size, input_length=1, embeddings_regularizer=regularizer)(item_input)
+    item_embedding = Embedding(num_items, embedding_size, embeddings_regularizer=regularizer)(item_input)
     item_vector = Flatten()(item_embedding)
     item_vector = Dropout(dropout_rate)(item_vector)
 
-    dot_product = Dot(axes=1)([user_vector, item_vector])
-    model = Model(inputs=[user_input, item_input], outputs=dot_product)
-    model.compile(optimizer=Adam(), loss='mse')
+    # Brand input and embedding
+    brand_input = Input(shape=(1,))
+    brand_embedding = Embedding(num_brands, embedding_size, embeddings_regularizer=regularizer)(brand_input)
+    brand_vector = Flatten()(brand_embedding)
+    brand_vector = Dropout(dropout_rate)(brand_vector)
+
+    # Gender input and embedding
+    gender_input = Input(shape=(1,))
+    gender_embedding = Embedding(num_genders, embedding_size, embeddings_regularizer=regularizer)(gender_input)
+    gender_vector = Flatten()(gender_embedding)
+    gender_vector = Dropout(dropout_rate)(gender_vector)
+
+    # Combine item and brand vectors
+    combined_item_brand_vector = Concatenate()([item_vector, brand_vector])
+
+    combined_with_gender_vector = Concatenate()([combined_item_brand_vector, gender_vector])
+
+    combined_vector = Concatenate()([user_vector, combined_with_gender_vector])
+    
+    output = Dense(1, activation='sigmoid')(combined_vector)
+
+    model = Model(inputs=[user_input, item_input, brand_input, gender_input], outputs=output)
+    model.compile(optimizer=Adam(learning_rate=0.001), loss='binary_crossentropy')
+
     return model
+
 
 @app.route('/recommend', methods=['POST'])
 def recommend():
+    global model
     data = request.get_json()
-    user_name = data['user_name']
+    user_id = data['user_id']
     top_n = data.get('top_n', 25)
-    brands = data.get('brands')
-    
-    allowed_items = {item["productID"]: i for i, item in enumerate(clothing_data) if item['brand'] in brands}
+    gender = data['gender']
 
+    if user_id not in usernames:
+        raise ValueError(f"User {user_id} not found in the data.")
 
-    user_id = usernames[user_name]
-    user_vector = model.layers[2].get_weights()[0][user_id]
-    item_vectors = model.layers[3].get_weights()[0]
+    user_index = usernames[user_id]
+
+    user_vector = model.get_layer('embedding').get_weights()[0][user_index]
+    item_vectors = model.get_layer('embedding_1').get_weights()[0]
 
     scores = np.dot(item_vectors, user_vector)
-    recommended_item_ids = np.argsort(scores)[::-1][:top_n]
-    recommended_items = [clothing_data[item_id] for item_id in recommended_item_ids if item_id in allowed_items.values()]
-            
+
+    recommended_item_indices = np.argsort(scores)[::-1][:top_n]
+
+    recommended_items = []
+
+    for i in recommended_item_indices:
+        if (len(recommended_items) == top_n):
+            break
+        if clothing_data[i]['gender'] == gender or clothing_data[i]['gender'] == 'Unisex':
+            recommended_items.append(clothing_data[i])
+        
         
 
     return jsonify(recommended_items)
@@ -73,87 +113,145 @@ def recommend():
 @app.route('/createUser', methods=['POST'])
 def create_user():
     data = request.get_json()
-    user_name = data['user_name']
+    user_id = data['user_id']
     
-    if user_name in usernames:
+    if user_id in usernames:
         return jsonify({"message": "User already exists!"})
 
     user_id = len(usernames)
-    usernames[user_name] = user_id
+    usernames[user_id] = user_id
 
     return jsonify({"message": "User created!"})
 
+# Route to update the model with new data
 @app.route('/update', methods=['POST'])
 def update():
     data = request.get_json()
-    new_user_data = data['new_user_data']
+    user = data['user']
 
     user_ids = []
     item_ids = []
+    brand_ids = []
+    gender_ids = []
     labels = []
-    for user in new_user_data:
-        user_id = usernames.get(user["id"])
-        if user_id is None:
-            user_id = len(usernames)
-            usernames[user["id"]] = user_id
+    global model
+    # Update usernames and get user_id
+    user_id = usernames.get(user["id"])
+    if user_id is None:
+        user_id = len(usernames)
+        usernames[user["id"]] = user_id
+        with open('./data/usernames.json', 'w') as f:
+            json.dump(usernames, f)
 
-        for item in user["liked_items"]:
-            item_id = clothes.get(item["productID"])
-            if item_id is None:
-                item_id = len(clothes)
-                clothes[item["productID"]] = item_id
-            user_ids.append(user_id)
-            item_ids.append(item_id)
-            labels.append(1)
+    for item in user["liked_items"]:
+        item_id = clothes.get(item["productID"])
+        if item_id is None:
+            item_id = len(clothes)
+            clothes[item["productID"]] = item_id
 
-        for item in user["disliked_items"]:
-            item_id = clothes.get(item["productID"])
-            if item_id is None:
-                item_id = len(clothes)
-                clothes[item["productID"]] = item_id
-            user_ids.append(user_id)
-            item_ids.append(item_id)
-            labels.append(-1)
+        brand_id = brands.get(item["brand"])
+        if brand_id is None:
+            brand_id = len(brands)
+            brands[item["brand"]] = brand_id
+
+        user_ids.append(user_id)
+        item_ids.append(item_id)
+        brand_ids.append(brand_id)
+        labels.append(1)
+
+        gender_id = genders.get(user['gender'])
+        if gender_id is None:
+            gender_id = len(genders)
+            genders[user['gender']] = gender_id
+        gender_ids.append(gender_id)
+
+    for item in user["disliked_items"]:
+        item_id = clothes.get(item["productID"])
+        if item_id is None:
+            item_id = len(clothes)
+            clothes[item["productID"]] = item_id
+
+        brand_id = brands.get(item["brand"])
+        if brand_id is None:
+            brand_id = len(brands)
+            brands[item["brand"]] = brand_id
+
+        user_ids.append(user_id)
+        item_ids.append(item_id)
+        brand_ids.append(brand_id)
+        labels.append(0)
+
+        gender_id = genders.get(user['gender'])
+        if gender_id is None:
+            gender_id = len(genders)
+            genders[user['gender']] = gender_id
+        gender_ids.append(gender_id)
 
     user_ids_array = np.array(user_ids)
     item_ids_array = np.array(item_ids)
+    brand_ids_array = np.array(brand_ids)
+    gender_ids_array = np.array(gender_ids)
     labels_array = np.array(labels)
-
-    # Debugging: Print the shapes of the arrays
-    print(f'user_ids_array shape: {user_ids_array.shape}')
-    print(f'item_ids_array shape: {item_ids_array.shape}')
-    print(f'labels_array shape: {labels_array.shape}')
 
     # Check if the arrays are not empty
     if user_ids_array.size == 0 or item_ids_array.size == 0 or labels_array.size == 0:
         return jsonify({"error": "No data to update"}), 400
 
-    # Determine the new size of the embeddings
-    max_user_id = user_ids_array.max()
-    max_item_id = item_ids_array.max()
-    num_users = max(max_user_id + 1, len(usernames))
-    num_items = max(max_item_id + 1, len(clothes))
+    # Gather all old and new entities
+    num_old_users = model.get_layer(name='embedding').input_dim
+    num_old_items = model.get_layer(name='embedding_1').input_dim
+    num_old_brands = model.get_layer(name='embedding_2').input_dim
+    num_old_genders = model.get_layer(name='embedding_3').input_dim
+
+    num_new_users = len(usernames)
+    num_new_items = len(clothes)
+    num_new_brands = len(brands)
+    num_new_genders = len(genders)
+
+    # Total number of entities
+    num_users = max(num_old_users, num_new_users)
+    num_items = max(num_old_items, num_new_items)
+    num_brands = max(num_old_brands, num_new_brands)
+    num_genders = max(num_old_genders, num_new_genders)
+
+    # Debugging: Print embedding sizes
+    print(f"num_old_users: {num_old_users}, num_new_users: {num_new_users}, num_users: {num_users}")
+    print(f"num_old_items: {num_old_items}, num_new_items: {num_new_items}, num_items: {num_items}")
+    print(f"num_old_brands: {num_old_brands}, num_new_brands: {num_new_brands}, num_brands: {num_brands}")
+    print(f"num_old_genders: {num_old_genders}, num_new_genders: {num_new_genders}, num_genders: {num_genders}")
 
     # Create a new model with updated embedding sizes
-    new_model = create_model(num_users, num_items)
+    new_model = create_model(num_users, num_items, num_brands, num_genders)
 
     # Transfer weights
-    old_user_weights = model.layers[2].get_weights()[0]
-    new_user_weights = np.zeros((num_users, old_user_weights.shape[1]))
+    old_user_weights = model.get_layer(name='embedding').get_weights()[0]
+    new_user_weights = new_model.get_layer(name='embedding').get_weights()[0]
     new_user_weights[:old_user_weights.shape[0], :] = old_user_weights
 
-    old_item_weights = model.layers[3].get_weights()[0]
-    new_item_weights = np.zeros((num_items, old_item_weights.shape[1]))
+    old_item_weights = model.get_layer(name='embedding_1').get_weights()[0]
+    new_item_weights = new_model.get_layer(name='embedding_1').get_weights()[0]
     new_item_weights[:old_item_weights.shape[0], :] = old_item_weights
 
-    new_model.layers[2].set_weights([new_user_weights])
-    new_model.layers[3].set_weights([new_item_weights])
+    old_brand_weights = model.get_layer(name='embedding_2').get_weights()[0]
+    new_brand_weights = new_model.get_layer(name='embedding_2').get_weights()[0]
+    new_brand_weights[:old_brand_weights.shape[0], :] = old_brand_weights
+
+    old_gender_weights = model.get_layer(name='embedding_3').get_weights()[0]
+    new_gender_weights = new_model.get_layer(name='embedding_3').get_weights()[0]
+    new_gender_weights[:old_gender_weights.shape[0], :] = old_gender_weights
+
+    new_model.get_layer(name='embedding').set_weights([new_user_weights])
+    new_model.get_layer(name='embedding_1').set_weights([new_item_weights])
+    new_model.get_layer(name='embedding_2').set_weights([new_brand_weights])
+    new_model.get_layer(name='embedding_3').set_weights([new_gender_weights])
 
     # Incrementally train the new model
-    new_model.fit([user_ids_array, item_ids_array], labels_array, epochs=1, verbose=1)
+    new_model.fit([user_ids_array, item_ids_array, brand_ids_array, gender_ids_array], labels_array, epochs=1, verbose=1)
 
     # Save the updated model
     new_model.save(model_path)
+
+    model = new_model
     
     return jsonify({"message": "Model updated successfully!"})
 
--- main.py
+++ main.py
@@ -2,12 +2,15 @@ import pymongo
 import certifi
 import requests
 import os
-from dotenv import load_dotenv
+import json
+
+from dotenv import load_dotenv, find_dotenv
 from supabase import create_client, Client
-from tensorflow import keras
+
+
 
 from keras.models import Model
-from keras.layers import Embedding, Flatten, Dot, Input, Activation
+from keras.layers import Embedding, Flatten, Dot, Input, Activation, Concatenate, Dense
 from keras.regularizers import l2
 from keras.optimizers import SGD, Adam
 from keras.initializers import RandomNormal
@@ -21,10 +24,12 @@ import numpy as np
 import pandas as pd
 
 
-class PreProcessData:
+
+
+class FetchData:
     def __init__(self) -> None:
         
-        load_dotenv('env/.env')
+        load_dotenv(find_dotenv())
         mongodb_url = os.getenv("MONGODB_URL")
         self.mongodb_client = pymongo.MongoClient(mongodb_url, tlsCAFile=certifi.where())
         
@@ -39,30 +44,24 @@ class PreProcessData:
             "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
         }   
     
-    def fetch_clothing_data(self) -> list:
+    def clothing_data(self) -> list:
         print("Fetching data from MongoDB")
         
         db = self.mongodb_client['my_data']
-        collection = db['brands_women']
-
         clothing_data = []
-        for item in collection.find():
-            data = db[f'{item["brand"].lower()}_women_tops']
-            for i in data.find():
-                clothing_data.extend(i['clothes_data'])
+        for i in db.list_collection_names():
+            collection = db[i]
+            for item in collection.find():
+                    clothing_data.extend(item['clothes_data'])
         
         return clothing_data
 
-    def fetch_user_data(self) -> list:
+    def user_data(self) -> list:
         print("Fetching data from Supabase")
         
-        user_data = self.supabase_client.from_('users').select('id','name','liked_items', 'disliked_items').execute()
-        return user_data.data
-
-    def data_cleaning(self, data: list) -> list:
-        print("Cleaning data")
+        user_data = self.supabase_client.from_('users').select('id','name','liked_items', 'disliked_items', 'gender').execute()
         
-        for i in data:
+        for i in user_data.data:
             i['liked_items'] = [
                 {
                     "name": j['name'],
@@ -79,73 +78,140 @@ class PreProcessData:
                 }
                 for j in i['disliked_items']
             ]
-        return data
+        return user_data.data
 
 class SwipeStyle_AI:
     def __init__(self, clothing_data: list, user_data: list) -> None:
+
+
         self.usernames = {user["id"]: i for i, user in enumerate(user_data)}
         self.clothes = {item["productID"]: i for i, item in enumerate(clothing_data)}
         
+        unique_brands = list(set(item["brand"] for item in clothing_data))  
+        self.brands = {brand: i for i, brand in enumerate(unique_brands)}
+
+        unique_genders = list(set(item["gender"] for item in clothing_data))
+        self.genders = {gender: i for i, gender in enumerate(unique_genders)}
+        
         self.num_users = len(self.usernames)
         self.num_clothes = len(self.clothes)
+        self.num_brands = len(self.brands)
+        self.num_genders = len(self.genders)
+
+        #Save data for API Use
+        with open('./data/usernames.json', 'w') as f:
+            json.dump(self.usernames, f)
+        
+        with open('./data/clothes.json', 'w') as f:
+            json.dump(self.clothes, f)
+
+        with open('./data/brands.json', 'w') as f:
+            json.dump(self.brands, f)
+
+        with open('./data/genders.json', 'w') as f:
+            json.dump(self.genders, f)
+
+
 
         embedding_size = 50
-        regularizer = l2(1e-4)  # Reduced regularization
+        regularizer = l2(1e-4)
 
         user_input = Input(shape=(1,))
-        user_embedding = Embedding(self.num_users, embedding_size, embeddings_initializer=RandomNormal(mean=0.0, stddev=0.05), embeddings_regularizer=regularizer)(user_input)
+        user_embedding = Embedding(self.num_users, embedding_size, 
+                                   embeddings_initializer=RandomNormal(mean=0.0, stddev=0.05), 
+                                   embeddings_regularizer=regularizer)(user_input)
         user_vector = Flatten()(user_embedding)
 
         item_input = Input(shape=(1,))
-        item_embedding = Embedding(self.num_clothes, embedding_size, embeddings_initializer=RandomNormal(mean=0.0, stddev=0.05), embeddings_regularizer=regularizer)(item_input)
+        item_embedding = Embedding(self.num_clothes, embedding_size, 
+                                   embeddings_initializer=RandomNormal(mean=0.0, stddev=0.05), 
+                                   embeddings_regularizer=regularizer)(item_input)
         item_vector = Flatten()(item_embedding)
 
-        dot_product = Dot(axes=1)([user_vector, item_vector])
-        output = Activation('sigmoid')(dot_product)  
+        brand_input = Input(shape=(1,))
+        brand_embedding = Embedding(self.num_brands, embedding_size, 
+                                    embeddings_initializer=RandomNormal(mean=0.0, stddev=0.05), 
+                                    embeddings_regularizer=regularizer)(brand_input)
+        brand_vector = Flatten()(brand_embedding)
 
-        self.model = Model(inputs=[user_input, item_input], outputs=output)
-        self.model.compile(optimizer=Adam(learning_rate=0.001), loss='binary_crossentropy')  # Adjusted learning rate and loss function
+        gender_input = Input(shape=(1,))
+        gender_embedding = Embedding(self.num_genders, embedding_size,
+                                     embeddings_initializer=RandomNormal(mean=0.0, stddev=0.5),
+                                     embeddings_regularizer=regularizer)(gender_input)
+        gender_vector = Flatten()(gender_embedding)
+        
+        
+
+        # Combine item and brand vectors
+        combined_item_brand_vector = Concatenate()([item_vector, brand_vector])
+
+        combined_with_gender_vector = Concatenate()([combined_item_brand_vector, gender_vector])
+
+        combined_vector = Concatenate()([user_vector, combined_with_gender_vector])
+        
+        output = Dense(1, activation='sigmoid')(combined_vector)
+
+        self.model = Model(inputs=[user_input, item_input, brand_input, gender_input], outputs=output)
+        self.model.compile(optimizer=Adam(learning_rate=0.001), loss='binary_crossentropy')
+
+        
 
     def prepare_training_data(self, user_data: list):
         user_ids = []
         item_ids = []
+        brand_ids = []
         labels = []
+        genders = []
 
         for user in user_data:
             user_id = self.usernames[user["id"]]
             for item in user["liked_items"]:
                 item_id = self.clothes[item["productID"]]
+                brand_id = self.brands[item["brand"]]
                 user_ids.append(user_id)
                 item_ids.append(item_id)
+                brand_ids.append(brand_id)
                 labels.append(1)
+                try:
+                    genders.append(self.genders[item['gender']])
+                except:
+                    genders.append(self.genders[user['gender']])
+                
             for item in user["disliked_items"]:
                 item_id = self.clothes[item["productID"]]
+                brand_id = self.brands[item["brand"]]
                 user_ids.append(user_id)
                 item_ids.append(item_id)
-                labels.append(0)  # Use 0 for dislikes to match sigmoid output
+                brand_ids.append(brand_id)
+                labels.append(0)
+                try:
+                    genders.append(self.genders[item['gender']])
+                except:
+                    genders.append(self.genders[user['gender']])
+                
 
         user_ids_array = np.array(user_ids)
         item_ids_array = np.array(item_ids)
+        brand_ids_array = np.array(brand_ids)
+        genders_array = np.array(genders)
         labels = np.array(labels)
         
-        return user_ids_array, item_ids_array, labels
+        return user_ids_array, item_ids_array, brand_ids_array, genders_array, labels
 
     def train_model(self, user_data: list, epochs=100) -> None:
-        user_ids_array, item_ids_array, labels = self.prepare_training_data(user_data)
+        user_ids_array, item_ids_array, brand_ids_array, genders_array, labels = self.prepare_training_data(user_data)
 
-        # Split data into training and test sets
         X_train, X_test, y_train, y_test = train_test_split(
-            np.column_stack((user_ids_array, item_ids_array)), labels, test_size=0.25
+            np.column_stack((user_ids_array, item_ids_array, brand_ids_array, genders_array)), labels, test_size=0.25
         )
 
         history = self.model.fit(
-            [X_train[:, 0], X_train[:, 1]], y_train,
+            [X_train[:, 0], X_train[:, 1], X_train[:, 2], X_train[:, 3]], y_train,
             epochs=epochs,
             verbose=1,
             validation_split=0.25
         )
 
-        # Plot training history
         plt.plot(history.history['loss'], label='Training Loss')
         plt.plot(history.history['val_loss'], label='Validation Loss')
         plt.xlabel('Epochs')
@@ -153,16 +219,10 @@ class SwipeStyle_AI:
         plt.legend()
         plt.show()
 
-        # Evaluate the model
-        test_loss = self.model.evaluate([X_test[:, 0], X_test[:, 1]], y_test, verbose=1)
+        test_loss = self.model.evaluate([X_test[:, 0], X_test[:, 1], X_test[:, 2], X_test[:, 3]], y_test, verbose=1)
         print(f'Test Loss: {test_loss}')
 
-        # Predict and calculate metrics
-        y_pred_scores = self.model.predict([X_test[:, 0], X_test[:, 1]])
-        print(f"Predicted scores range: {y_pred_scores.min()} to {y_pred_scores.max()}")
-        print(f"Predicted scores mean: {y_pred_scores.mean()}")
-
-        # Experiment with different thresholds
+        y_pred_scores = self.model.predict([X_test[:, 0], X_test[:, 1], X_test[:, 2], X_test[:, 3]])
         optimal_threshold = 0.5  
         y_pred_binary = (y_pred_scores > optimal_threshold).astype(int)
         y_true_binary = y_test
@@ -173,32 +233,38 @@ class SwipeStyle_AI:
         print(f'Precision: {precision}')
         print(f'Recall: {recall}')
 
-        # Print label distribution
-        unique, counts = np.unique(labels, return_counts=True)
-        print(f"Label distribution: {dict(zip(unique, counts))}")
+    def recommend_items(self, user_id: str, top_n=3) -> list:
+        if user_id not in self.usernames:
+            raise ValueError(f"User {user_id} not found in the data.")
 
-    def recommend_items(self, user_name: str, top_n=3) -> list:
-        user_id = self.usernames[user_name]
-        user_vector = self.model.layers[2].get_weights()[0][user_id]
-        item_vectors = self.model.layers[3].get_weights()[0]
+        user_index = self.usernames[user_id]
+
+        # Get the user vector from the user embedding layer
+        user_vector = self.model.get_layer('embedding').get_weights()[0][user_index]
+
+        # Get all item vectors from the item embedding layer
+        item_vectors = self.model.get_layer('embedding_1').get_weights()[0]
 
         scores = np.dot(item_vectors, user_vector)
-        recommended_item_ids = np.argsort(scores)[::-1][:top_n]
-        recommended_items = [list(self.clothes.keys())[list(self.clothes.values()).index(item_id)] for item_id in recommended_item_ids]
+
+        # Get top N items with the highest scores
+        recommended_item_indices = np.argsort(scores)[::-1][:top_n]
+
+        # Map indices back to item IDs
+        recommended_items = [list(self.clothes.keys())[i] for i in recommended_item_indices]
 
         return recommended_items
 
     def save_model(self, model_name: str) -> None:
         self.model.save(model_name)
-    
 
 
 if __name__ == "__main__":
-    pp_data = PreProcessData()
-    clothing_data = pp_data.fetch_clothing_data()
-    raw_data = pp_data.fetch_user_data()
-    user_data = pp_data.data_cleaning(raw_data)
-    
+
+    fetch = FetchData()
+    clothing_data = fetch.clothing_data()
+    user_data = fetch.user_data()
+
     ss_ai = SwipeStyle_AI(clothing_data, user_data)
     ss_ai.train_model(user_data, epochs=100)
 
